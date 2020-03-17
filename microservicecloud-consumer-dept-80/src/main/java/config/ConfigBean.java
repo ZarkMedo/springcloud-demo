cf7fad9846d23abea64605b7cf6a66ae4bc66c08
git_by_medo_mac <medo_mac@github.com>
2020-03-17 13:18:02 +0800

    修改pom文件以及创建配置RestTemplate

---
+++ microservicecloud-consumer-dept-80/src/main/java/config/ConfigBean.java
@@ -0,0 +1,21 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName: ConfigBean
+ * @Auther: medo
+ * @Date: 2020-03-17 13:14
+ * @Description: TODO
+ **/
+@Configuration
+public class ConfigBean {
+
+    // 使用restTemplate更优雅的操作http间的调用
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
